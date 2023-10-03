1f03b0c7cd1a4049f3984fdbb96c1b016e4ea501
tomix421q <zilka.tomas421@gmail.com>
2023-10-03 15:46:31 +0100

    asd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -87,3 +87,5 @@ namespace myFirstApp.Controllers
         }
     }
 }
+
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,9 +21,14 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+
+
 
 app.Run();

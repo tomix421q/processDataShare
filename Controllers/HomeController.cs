4183afea3538ce5337e1f9f04b1c34c859d63c84
tomix421q <zilka.tomas421@gmail.com>
2023-10-04 00:52:24 +0200

    Utorok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-//using myFirstApp.Models;
 using S7.Net;
-using System.Data;
-using System.Diagnostics;
 
 
-namespace myFirstApp.Controllers
+namespace processDataShare.Controllers
 {
     public class HomeController : Controller
     {
@@ -16,43 +13,68 @@ namespace myFirstApp.Controllers
             _logger = logger;
         }
 
+        //10.184.159.108 ASQ5
+
         public IActionResult Index()
         {
+            Models.Asq5_model Asq5Model = new();
             {
-                //10.184.159.108 ASQ5
-                float ROB1_RefValue, ROB1_WeightTolMinus, ROB1_WeightTolPlus, ROB1_WeightActualValue, ROB1_Temperature, ROB1_SetTemperature, ROB1_TimeDrying, MixingTime;
+                //Predvolené hodnoty pre PLC parametre (FAKE)
+                Asq5Model.ROB1_RefValue = 0.0f;
+                Asq5Model.ROB1_WeightTolMinus = 0.0f;
+                Asq5Model.ROB1_WeightTolPlus = 0.0f;
+                Asq5Model.ROB1_WeightActualValue = 0.0f;
+                Asq5Model.ROB1_Temperature = 0.0f;
+                Asq5Model.ROB1_SetTemperature = 0.0f;
+                Asq5Model.ROB1_TimeDrying = 0.0f;
+                Asq5Model.MixingTime = 0.0f;
+                Asq5Model.ROB1_Downtime_Time = 0;
+                Asq5Model.ROB1_FormNumber = 0;
+                Asq5Model.ROB1_GoWeightAfter = 0;
 
-                int ROB1_Downtime_Time, ROB1_FormNumber, ROB1_GoWeightAfter;
 
                 using (var plc = new Plc(CpuType.S71500, "10.184.159.108", 0, 1))
+
                 {
-                    plc.Open(); //Connect?
 
-                    ROB1_Downtime_Time = ((ushort)plc.Read("DB179.DBW0.0")).ConvertToShort();
-                    ROB1_FormNumber = ((ushort)plc.Read("DB179.DBW2.0")).ConvertToShort();
-                    ROB1_GoWeightAfter = ((ushort)plc.Read("DB179.DBW0.0")).ConvertToShort();
 
-                    ROB1_RefValue = ((uint)plc.Read("DB179.DBD6.0")).ConvertToFloat();
-                    ROB1_WeightTolMinus = ((uint)plc.Read("DB179.DBD10.0")).ConvertToFloat();
-                    ROB1_WeightTolPlus = ((uint)plc.Read("DB179.DBD14.0")).ConvertToFloat();
-                    ROB1_WeightActualValue = ((uint)plc.Read("DB179.DBD18.0")).ConvertToFloat();
-                    ROB1_Temperature = ((uint)plc.Read("DB179.DBD22.0")).ConvertToFloat();
-                    ROB1_SetTemperature = ((uint)plc.Read("DB179.DBD26.0")).ConvertToFloat();
-                    ROB1_TimeDrying = ((uint)plc.Read("DB179.DBD30.0")).ConvertToFloat();
-                    MixingTime = ((uint)plc.Read("DB179.DBD34.0")).ConvertToFloat();
+                    try
+                    {
+
+                        plc.Open(); //Connect
+                        Asq5Model.ROB1_Downtime_Time = ((ushort)plc.Read("DB179.DBW0.0")).ConvertToShort();
+                        Asq5Model.ROB1_FormNumber = ((ushort)plc.Read("DB179.DBW2.0")).ConvertToShort();
+                        Asq5Model.ROB1_GoWeightAfter = ((ushort)plc.Read("DB179.DBW0.0")).ConvertToShort();
+                        Asq5Model.ROB1_RefValue = ((uint)plc.Read("DB179.DBD6.0")).ConvertToFloat();
+                        Asq5Model.ROB1_WeightTolMinus = ((uint)plc.Read("DB179.DBD10.0")).ConvertToFloat();
+                        Asq5Model.ROB1_WeightTolPlus = ((uint)plc.Read("DB179.DBD14.0")).ConvertToFloat();
+                        Asq5Model.ROB1_WeightActualValue = ((uint)plc.Read("DB179.DBD18.0")).ConvertToFloat();
+                        Asq5Model.ROB1_Temperature = ((uint)plc.Read("DB179.DBD22.0")).ConvertToFloat();
+                        Asq5Model.ROB1_SetTemperature = ((uint)plc.Read("DB179.DBD26.0")).ConvertToFloat();
+                        Asq5Model.ROB1_TimeDrying = ((uint)plc.Read("DB179.DBD30.0")).ConvertToFloat();
+                        Asq5Model.MixingTime = ((uint)plc.Read("DB179.DBD34.0")).ConvertToFloat();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine("Chyba pri pripájaní k PLC: " + ex.Message);
+                    }
+
+
+
 
 
                 }
 
-                ViewBag.ROB1_Downtime_Time = ROB1_Downtime_Time;
-                ViewBag.ROB1_RefValue = ROB1_RefValue;
-                ViewBag.ROB1_WeightTolMinus = ROB1_WeightTolMinus;
-                ViewBag.ROB1_WeightTolPlus = ROB1_WeightTolPlus;
-                ViewBag.ROB1_WeightActualValue = ROB1_WeightActualValue;
-                ViewBag.ROB1_Temperature = ROB1_Temperature;
-                ViewBag.ROB1_SetTemperature = ROB1_SetTemperature;
-                ViewBag.ROB1_TimeDrying = ROB1_TimeDrying;
-                ViewBag.MixingTime = MixingTime;
+                //ViewBag.ROB1_Downtime_Time = ROB1_Downtime_Time;
+                //ViewBag.ROB1_RefValue = ROB1_RefValue;
+                //ViewBag.ROB1_WeightTolMinus = ROB1_WeightTolMinus;
+                //ViewBag.ROB1_WeightTolPlus = ROB1_WeightTolPlus;
+                //ViewBag.ROB1_WeightActualValue = ROB1_WeightActualValue;
+                //ViewBag.ROB1_Temperature = ROB1_Temperature;
+                //ViewBag.ROB1_SetTemperature = ROB1_SetTemperature;
+                //ViewBag.ROB1_TimeDrying = ROB1_TimeDrying;
+                //ViewBag.MixingTime = MixingTime;
 
 
 
@@ -60,30 +82,36 @@ namespace myFirstApp.Controllers
 
 
                 ///////////////////////////////////////////////////////////////
-                int rightPart, leftPart, actualStep;
-                short actualDowntime;
+                //int rightPart, leftPart, actualStep;
+                //short actualDowntime;
 
-                using (var plc = new Plc(CpuType.S71500, "10.184.159.45", 0, 1))
-                {
-                    plc.Open();
+                //using (var plc = new Plc(CpuType.S71500, "10.184.159.45", 0, 1))
+                //{
+                //    plc.Open();
 
-                    rightPart = ((uint)plc.Read("DB26.DBD2.0")).ConvertToInt();
-                    leftPart = ((uint)plc.Read("DB26.DBD6.0")).ConvertToInt();
-                    actualStep = ((uint)plc.Read("DB26.DBD10.0")).ConvertToInt();
-                    actualStep = ((uint)plc.Read("DB26.DBD10.0")).ConvertToInt();
-                    actualDowntime = ((ushort)plc.Read("DB26.DBW0.0")).ConvertToShort();
+                //    rightPart = ((uint)plc.Read("DB26.DBD2.0")).ConvertToInt();
+                //    leftPart = ((uint)plc.Read("DB26.DBD6.0")).ConvertToInt();
+                //    actualStep = ((uint)plc.Read("DB26.DBD10.0")).ConvertToInt();
+                //    actualStep = ((uint)plc.Read("DB26.DBD10.0")).ConvertToInt();
+                //    actualDowntime = ((ushort)plc.Read("DB26.DBW0.0")).ConvertToShort();
 
-                }
+                //}
 
 
-                ViewBag.pravydiel = rightPart;
-                ViewBag.lavydiel = leftPart;
-                ViewBag.aktualnyKrok = actualStep;
-                ViewBag.prestoj = actualDowntime;
+                //ViewBag.pravydiel = rightPart;
+                //ViewBag.lavydiel = leftPart;
+                //ViewBag.aktualnyKrok = actualStep;
+                //ViewBag.prestoj = actualDowntime;
 
-                return View();
+                return View(Asq5Model);
 
             }
+        }
+        public IActionResult Asq5()
+        {
+            Models.Asq5_model Asq5Model = new();
+            // Inicializácia modelu a prípadné nastavenie hodnôt
+            return View(Asq5Model);
         }
     }
 }
---
+++ Models/Asq5_model.cs
@@ -0,0 +1,17 @@
+﻿namespace processDataShare.Models
+{
+    public class Asq5_model
+    {
+        public float ROB1_RefValue { get; set; }
+        public float ROB1_WeightTolMinus { get; set; }
+        public float ROB1_WeightTolPlus { get; set; }
+        public float ROB1_WeightActualValue { get; set; }
+        public float ROB1_Temperature { get; set; }
+        public float ROB1_SetTemperature { get; set; }
+        public float ROB1_TimeDrying { get; set; }
+        public float MixingTime { get; set; }
+        public int ROB1_Downtime_Time { get; set; }
+        public int ROB1_FormNumber { get; set; }
+        public int ROB1_GoWeightAfter { get; set; }
+    }
+}
